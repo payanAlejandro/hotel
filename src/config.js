76305ae113a5d47d5af60f23f1c164e8925b8fdd
Alejandro Payan <alejandro.payan@sigob.com.mx>
2023-10-09 18:06:21 -0700

    fix de conexion sin docker

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ config();
 
 export const PORT = process.env.PORT || 3000;
 export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "12345678";
+export const DB_USER = process.env.DB_USER || "hotel";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "Hotel123";
 export const DB_DATABASE = process.env.DB_DATABASE || "Hotel";
 export const DB_PORT = process.env.DB_PORT || 3306;
